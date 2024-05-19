d6815d9ffe17cbea1449349aca89dafd2171e7a3
majufigueiredo <143432089+majufigueiredo@users.noreply.github.com>
2024-05-19 14:43:07 -0300

    Update estrelas.michelin.py

--- estrelas.michelin.py
+++ estrelas.michelin.py
@@ -1,16 +1,19 @@
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-csv_url = 'https://raw.githubusercontent.com/majufigueiredo/Estrela_Michelin/main/one-star-michelin-restaurants.csv'
+
+csv_url = 'https://raw.githubusercontent.com/seu-usuario/seu-repositorio/main/one-star-michelin-restaurants.csv'
+
 
 df = pd.read_csv(csv_url)
+
 
 st.write(df.head())
 
 region_count = df['region'].value_counts().reset_index()
 region_count.columns = ['region', 'count']
+
 
 plt.figure(figsize=(10, 6))
 plt.bar(region_count['region'], region_count['count'], color='skyblue')
@@ -19,6 +22,7 @@ plt.ylabel('Número de Restaurantes Premiados')
 plt.title('Número de Restaurantes com Uma Estrela Michelin por Região')
 plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
+
 
 st.pyplot(plt)
 
